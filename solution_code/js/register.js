1c5dd19aef60c00ac7a524beb8402390cddcc2e5
brobertson3 <TalkNerdyToMe@Brents-MacBook-Pro.local>
2016-04-24 18:25:20 -0400

    Quick changes before implementing JS
    Confirm the email addresses instead. Added an “other” option in the
    dropdown selection menu.

--- solution_code/js/register.js
+++ solution_code/js/register.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-	
+	/* TODO: 1. Check to make sure that no field is left blank. If blank, display
+				empty field in a red border with text that says to enter in info
+	         2. Make sure that both email fields match. If not, highlight both
+	            email fields in red and show message that says they much match
+	         3. If checked, then show the password */
 });
