f7e4adf458ea1bef4b10edd13e64f4b1b03e3e3e
brobertson3 <TalkNerdyToMe@Brents-MacBook-Pro.local>
2016-04-26 14:30:33 -0400

    Email validation
    Checks if the email addresses are the same. If not, then displays a
    prompt to the user.  Also, prompt only shows if both email fields are
    filled in.
    
    More comments added.

--- solution_code/js/register.js
+++ solution_code/js/register.js
@@ -11,6 +11,8 @@ $(document).ready(function () {
 	function validateForm (event) {
 		event.preventDefault();
 
+		/* variables have to be updated locally in order to get the
+		   new values stored in the field upon submit */
 		var username = $("#username").val();
 		var email_original = $('#email_address').val();
 		var email_confirm = $('#email_confirm').val();
@@ -33,19 +35,46 @@ $(document).ready(function () {
 			$('input#password_orig').addClass('error'); //.after("<span class='error_text'>No input left behind! Please enter in a password.</span>");
 			$('#error_password').css("visibility", "visible");
 		} 
+
+		/* Only show this prompt when both email fields are filled in */
+		if (email_original != '' && email_confirm != '') {
+			/* Now check to see if the email addresses match */
+			confirmEmail(email_original, email_confirm);
+		}
+		
 	}
 
+	/* Resets the form back to normal before checking fields */
 	function resetAllForms () {
 		$('input#username, input#email_address, input#email_confirm, input#password_orig').removeClass('error');
 		$('#error_username, #error_email, #error_email_confirm, #error_password').css("visibility", "hidden");
 	}
 
-	function togglePassword (event) {
+	/* Switches the visibility of the password based on if the box is checked */
+	function togglePassword () {
 		// if checked then show the password (apparently .checked() doesn't work anymore?)
 		if($(this).prop("checked")) {
 			$('#password_orig').attr("type", "text");
 		} else {
 			$('#password_orig').attr("type", "password");
+		}
+	}
+
+	/* Compare the two email fields to make sure that they match */
+	function confirmEmail (original, confirm) {
+		var emailConfirm = $('#email_confirm');
+		var confirmPrompt = $('#confirm_prompt');
+		/* If they don't match, then show a prompt alerting the user. This should
+		   persist until the inputs match */
+		   console.log(original + " <> " + confirm);
+
+		/* If they don't match and the confirm_prompt isn't visible */
+		if ((original != confirm) && (!confirmPrompt.length)) {
+			emailConfirm.after('<p class="error_text" id ="confirm_prompt">Make sure email addresses match!!! Sorry, I didnt mean to yell.</p>');
+			$('#confirm_prompt').css("visibility", "visible");
+		} else if ((original == confirm)) {
+			/* Now we can remove the prompt since they match */
+			confirmPrompt.remove();
 		}
 	}
 });
