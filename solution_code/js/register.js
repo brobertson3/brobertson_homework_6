970bc2b7d1ed68e5dc204b4df6733342ab6ffdff
brobertson3 <TalkNerdyToMe@Brents-MacBook-Pro.local>
2016-04-26 11:09:27 -0400

    Added form validation for blank fields
    When a field is blank in the form, the input box is highlighted red and
    a prompt appears alerting the user to enter in a value.

--- solution_code/js/register.js
+++ solution_code/js/register.js
@@ -4,4 +4,31 @@ $(document).ready(function () {
 	         2. Make sure that both email fields match. If not, highlight both
 	            email fields in red and show message that says they much match
 	         3. If checked, then show the password */
+
+	$('form').submit(validateForm);
+
+	function validateForm(event) {
+		event.preventDefault();
+
+		var username = $("#username").val();
+		var email_original = $('#email_address').val();
+		var email_confirm = $('#email_confirm').val();
+		var password = $('#password_orig').val();
+
+		/* use all if statements so that we can check if all are non-empty and show
+		   all error messages at the same time */
+		if (username == '' ) {
+			$('input#username').addClass('error');
+			$('#error_username').css("visibility", "visible"); //.after("<span class='error_text'>No input left behind! Please enter in a username.</span>");
+		} if (email_original == '') {
+			$('input#email_address').addClass('error'); //.after("<span class='error_text'>No input left behind! Please enter in an email address.</span>");
+			$('#error_email').css("visibility", "visible");
+		} if (email_confirm == '') {
+			$('input#email_confirm').addClass('error'); //.after("<span class='error_text'>No input left behind! Please enter in an email address.</span>");
+			$('#error_email_confirm').css("visibility", "visible");
+		} if (password == '') {
+			$('input#password_orig').addClass('error'); //.after("<span class='error_text'>No input left behind! Please enter in a password.</span>");
+			$('#error_password').css("visibility", "visible");
+		} 
+	}
 });
